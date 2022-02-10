5df0e2134f28be257ec51369ef790d0954b5f0e0
CBascue <codiebascue13@yahoo.com>
2022-02-10 11:51:56 -0500

    Initial commit

---
+++ src/main/java/Jade/LevelEditorScene.java
@@ -0,0 +1,54 @@
+    package Jade;
+
+    import components.Sprite;
+    import components.SpriteRenderer;
+    import components.Spritesheet;
+    import org.joml.Vector2f;
+    import org.joml.Vector4f;
+    import org.lwjgl.system.CallbackI;
+    import util.AssetPool;
+
+    public class LevelEditorScene extends Scene {
+
+        public LevelEditorScene() {
+
+        }
+
+        @Override
+        public void init() {
+            loadResources();
+
+            this.camera = new Camera(new Vector2f(-250, 0));
+
+            Spritesheet sprites = AssetPool.getSpritesheet("assets/images/spritesheet.png");
+
+
+            GameObject obj1 = new GameObject("Object 1", new Transform(new Vector2f(100, 100), new Vector2f(256, 256)));
+            obj1.addComponent(new SpriteRenderer(sprites.getSprite(0)));
+            this.addGameObjectToScene(obj1);
+
+            GameObject obj2 = new GameObject("Object 2", new Transform(new Vector2f(400, 100), new Vector2f(256, 256)));
+            obj2.addComponent(new SpriteRenderer(sprites.getSprite(15)));
+            this.addGameObjectToScene(obj2);
+        }
+
+        private void loadResources() {
+            AssetPool.getShader("assets/shaders/default.glsl");
+
+            AssetPool.addSpritesheet("assets/images/spritesheet.png",
+                    new Spritesheet(AssetPool.getTexture("assets/images/spritesheet.png"),
+                            16, 16, 26, 0));
+        }
+
+        @Override
+        public void update(float dt) {
+            System.out.println("FPS: " + (1.0f / dt));
+
+            for (GameObject go : this.gameObjects) {
+                go.update(dt);
+            }
+
+            this.renderer.render();
+        }
+
+    }
